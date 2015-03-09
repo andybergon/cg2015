28624ea3fb3ebb49623c205d00f497292be8a6c5
andybergon <andybergon@gmail.com>
2015-03-09 21:25:11 +0100

    exercise03.js

--- 20150309/exercise03.js
+++ 20150309/exercise03.js
@@ -1,2 +1,9 @@
-write a script containing the function fibonacci(i)
-that returns the i-th element of the Fibonacci's serie (apply memoization pattern)
+// write a script containing the function fibonacci(i) that returns the i-th element of the Fibonacci's serie (apply memoization pattern)
+function fibonacci(n) {
+	fibonacci[0] = 0;
+	fibonacci[1] = 1;
+	if(!(n in fibonacci)) {
+		fibonacci[n] = fibonacci(n-1) + fibonacci(n-2);
+	}
+	return fibonacci[n];
+}
