d8d8e166c7382802e32f8091d3d7160cdd44fb05
andybergon <andybergon@gmail.com>
2015-03-12 14:05:28 +0100

    fixed exercise03
    remember:
    for (x in y) {
    x[y];//per accedere
    }

--- 20150310/exercise03.js
+++ 20150310/exercise03.js
@@ -3,7 +3,7 @@ function capitalize(word) {
 	return word.substring(0,1).toUpperCase() + word.substring(1);
 }
 
-capitalize("andrea");
+//capitalize("andrea");
 
 //3b, write a function that capitalize each word of the following text:
 //"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -12,11 +12,13 @@ var text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiu
 function capitalizeEach(text) {
 	var textCap;
 	var splittedText;
+
+	textCap = "";
 	splittedText = text.split(" ");
 	for (word in splittedText) {
-		textCap += word.capitalize(word);
+		textCap += capitalize(splittedText[word]) + " ";
 	};
 	return textCap;
 }
 
-capitalizeEach(text);
+//capitalizeEach(text);
